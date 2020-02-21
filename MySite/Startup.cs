4b8f407ab8662efca20d12407546fc01773b553f
infopulse\Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-02-21 14:22:45 +0200

    Добавлен класс Config, (строка подключений)

--- MySite/Startup.cs
+++ MySite/Startup.cs
@@ -6,16 +6,20 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using MySite.Service;
 
 namespace MySite
 {
     public class Startup
     {
-        
+        public IConfiguration Configuration { get; }
+        public Startup(IConfiguration configuration) => Configuration = configuration;
         public void ConfigureServices(IServiceCollection services)
         {
+            Configuration.Bind("Project", new Config());
             services.AddControllersWithViews()
                 .SetCompatibilityVersion(CompatibilityVersion.Version_3_0).AddSessionStateTempDataProvider();
         }
