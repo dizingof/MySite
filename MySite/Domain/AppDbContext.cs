5a4923e3b6004d19120cbb387b3429c8cc562c47
infopulse\Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-02-21 17:28:13 +0200

    промежуточный

--- MySite/Domain/AppDbContext.cs
+++ MySite/Domain/AppDbContext.cs
@@ -19,17 +19,17 @@ namespace MySite.Domain
             base.OnModelCreating(modelBuilder);
             modelBuilder.Entity<IdentityRole>().HasData(new IdentityRole
             {
-                Id = "",
-                Name = "",
+                Id = "44546e06-8719-4ad8-b88a-f271ae9d6eab",
+                Name = "admin",
                 NormalizedName = "ADMIN"
             });
             modelBuilder.Entity<IdentityUser>().HasData(new IdentityUser
             {
-                Id = "",
-                UserName = "",
+                Id = "3b62472e-4f66-49fa-a20f-e7685b9565d8",
+                UserName = "admin",
                 NormalizedUserName = "ADMIN",
-                Email = "",
-                NormalizedEmail ="",
+                Email = "my@email.com",
+                NormalizedEmail ="MY@EMAIL.COM",
                 EmailConfirmed = true,
                 PasswordHash = new PasswordHasher<IdentityUser>().HashPassword(null, "superpassword"),
                 SecurityStamp = string.Empty
@@ -37,17 +37,28 @@ namespace MySite.Domain
             });
             modelBuilder.Entity<IdentityUserRole<string>>().HasData(new IdentityUserRole<string>
             {
-                RoleId = "",
-                UserId = ""
+                RoleId = "8af10569-b018-4fe7-a380-7d6a14c70b74",
+                UserId = "3b62472e-4f66-49fa-a20f-e7685b9565d8"
             });
 
             modelBuilder.Entity<TextField>().HasData(new TextField
             {
-                Id = new Guid(""),
+                Id = new Guid("63dc8fa6-07ae-4391-8916-e057f71239ce"),
                 CodeWord = "PageIndex",
                 Title = "Главная"
             });
-
+            modelBuilder.Entity<TextField>().HasData(new TextField
+            {
+                Id = new Guid("70bf165a-700a-4156-91c0-e83fce0a277f"),
+                CodeWord = "PageServices",
+                Title = "Наши услуги"
+            });
+            modelBuilder.Entity<TextField>().HasData(new TextField
+            {
+                Id = new Guid("4aa76a4cc59d-409a-84c1-06e6487a137a"),
+                CodeWord = "PageContacts",
+                Title = "Контакты"
+            });
         }
 
     }
---
+++ MySite/Domain/Repositories/Abstract/ITextFieldsRepository.cs
@@ -0,0 +1,18 @@
+﻿using MySite.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySite.Domain.Repositories.Abstract
+{
+    public interface ITextFieldsRepository
+    {
+        IQueryable<TextField> GetTextFields();
+        TextField GetTextFieldByld(Guid id);
+        TextField GetTextFieldByCodeWords(string codeWord);
+        void SaveTextField(TextField entity);
+        void DeleteTextField(Guid id);
+
+    }
+}
