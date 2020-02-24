e7fe863b50af029b57053530a57ed78fc6311ffd
infopulse\Roman.Nepomniashchyi <Roman.Nepomniashchyi@infopulse.com>
2020-02-24 11:45:02 +0200

    a

---
+++ MySite/Domain/Repositories/EntityFramework/EFServiceItemsRepository.cs
@@ -0,0 +1,39 @@
+﻿using MySite.Domain.Entities;
+using MySite.Domain.Repositories.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MySite.Domain.Repositories.EntityFramework
+{
+    public class EFServiceItemsRepository : IServiceItemsRepository
+    {
+        private readonly AppDbContext context;
+        public EFServiceItemsRepository(AppDbContext context)
+        {
+            this.context = context;
+        }
+        public IQueryable<SeviceItem> GetServiceItems()
+        {
+            return context.ServiceItems;
+        }
+        public  SeviceItem GetServiceItemById(Guid id)
+        {
+            return context.ServiceItems.FirstOrDefault(x => x.Id == id);
+        }
+        public void SaveServiceItem(SeviceItem entity)
+        {
+            if (entity.Id == default)
+                context.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Added;
+            else
+                context.Entry(entity).State = Microsoft.EntityFrameworkCore.EntityState.Modified;
+            context.SaveChanges();
+        }
+        public void DeleteServiceItem(Guid id)
+        {
+            context.ServiceItems.Remove(new SeviceItem() { Id = id });
+            context.SaveChanges();
+        }
+    }
+}
